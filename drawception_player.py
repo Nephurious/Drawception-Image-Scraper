07faa73866f12eac6a6cc9db8d0846c93ffd9381
Nephurious <106878235+Nephurious@users.noreply.github.com>
2022-06-20 21:34:15 +1000

    Add method to scrape drawing links from games

--- drawception_player.py
+++ drawception_player.py
@@ -1,3 +1,4 @@
+import time
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -40,3 +41,50 @@ class DrawceptionPlayer:
             logging.error("Unable to access {}. \
                            HTTP response: {}".format(self.url, page.status_code))
             return False
+
+    @staticmethod
+    def get_drawing_links_from_url(url):
+        """Gets all the links to drawings from the given url. 
+
+        Returns None when there is an error or when the page returns
+        'Nothing here yet'.
+        """
+        page = requests.get(url)
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.content, 'html.parser')
+            if len(soup.find('div', class_="thumbpanel-container").contents) == 1:
+                if soup.find('div', class_="thumbpanel-container").find('div') == None:
+                    return None
+            base_url = re.match("(.+\\.com)", url)[1]
+            a_tags = soup.find_all('a', class_="btn btn-sm btn-bright")
+            links = []
+            for tag in a_tags:
+                links.append(base_url + tag['href'])
+            return links
+        else:
+            logging.error("Unable to access {}. HTTP response: {}".format(url, page.status_code))
+            return None
+    
+    def scrape_drawing_links(self, use_drawings_tab=False, max_pages=100):
+        """Scrapes the links of all drawings made by this player.
+        
+        By default, we get drawing panels using the 'public games' tab.
+        Drawception drawings created after 2021-12-17 are partially broken. Broken
+        drawings do not appear in the 'Drawings' tab.
+
+        There also exists a bug in which we can bypass the 100 page limit by setting the
+        page to be negative. Page x and -x + 2 are the same.
+        """
+        url = ""
+        if use_drawings_tab:
+            url = self.url + "drawings/"
+        else:
+            url = self.url + "games/"
+        
+        for page in range(1, max_pages + 1):
+            new_links = DrawceptionPlayer.get_drawing_links_from_url(url + str(page))
+            if new_links == None:
+                break
+            self.drawings += new_links
+            time.sleep(2)
+        return self.drawings
