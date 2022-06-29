d9f5a70ae88e6f00aeb4bb4b67e2f94495aaf7f8
Nephurious <106878235+Nephurious@users.noreply.github.com>
2022-06-29 19:15:00 +1000

    Refactor and add ability to optionally save csv

--- scraper.py
+++ scraper.py
@@ -7,23 +7,33 @@ import logging
 import os
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Downloads Drawception Images of the player given the player url.")
+    parser = argparse.ArgumentParser(
+        description="Downloads Drawception Images of the player given the player url.")
     parser.add_argument('url', type=str, help="Player url")
-    parser.add_argument('-d', '--directory', default='./images/', type=str, help="Directory to save the images in")
-    parser.add_argument('-l', '--log', action='store_true', help="Enables the logging of DEBUG and INFO to the file \"scraper.log\"")
-    parser.add_argument('-p', '--pages', default=100, type=int, help="Number of pages of drawings to scrape.")
-    parser.add_argument('--use-drawing-tab', action='store_true', help="Scrape from the drawings tabs rather than the public games tab")
+    parser.add_argument('-d', '--directory', default='./images/', type=str,
+                        help="Directory to save the images in. Defaults to './images/'")
+    parser.add_argument('-l', '--log', action='store_true',
+                        help="Enables the logging of DEBUG and INFO to the file \"scraper.log\"")
+    parser.add_argument('-p', '--pages', default=100, type=int,
+                        help="Number of pages of drawings to scrape.")
+    parser.add_argument('--use-drawing-tab', action='store_true',
+                        help="Scrape from the drawings tabs rather than the public games tab")
+    parser.add_argument('--save-csv-summary', action='store_true',
+                        help="Saves a csv summary of all the panels downloaded, including the id, name, creation_date, time_spent, panel_url.")
     args = parser.parse_args()
     if args.log:
-        logging.basicConfig(filename='scraper.log', level=logging.DEBUG, filemode='w')
+        logging.basicConfig(filename='scraper.log',
+                            level=logging.DEBUG, filemode='w')
     if not os.path.isdir(args.directory):
-        raise FileNotFoundError("No such or invalid directory: {}".format(args.directory))
+        raise FileNotFoundError(
+            "No such or invalid directory: {}".format(args.directory))
     player = DrawceptionPlayer(args.url)
     player.set_player_details()
     panel_df_rows = []
     try:
         for p in range(1, args.pages + 1):
-            links = player.scrape_drawing_links_by_page(p, args.use_drawing_tab)
+            links = player.scrape_drawing_links_by_page(
+                p, args.use_drawing_tab)
             if links == None:
                 break
             for link in links:
@@ -39,7 +49,9 @@ if __name__ == "__main__":
     except Exception as e:
         logging.exception(e)
     finally:
-        panel_df = pd.DataFrame(panel_df_rows, columns=['id','name',
-            'name_file_safe','author','creation_date','time_spent',
-            'panel_url'])
-        panel_df.to_csv("{}_panels.csv".format(player.name_filesafe), index=False)
+        if args.save_csv_summary and len(panel_df_rows) > 0:
+            panel_df = pd.DataFrame(panel_df_rows, 
+                                    columns=['id', 'name', 'creation_date',
+                                             'time_spent','panel_url'])
+            panel_df.to_csv("{}_panels.csv".format(
+                player.name_filesafe), index=False)
